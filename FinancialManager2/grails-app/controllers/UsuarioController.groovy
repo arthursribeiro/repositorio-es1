b14907b6df6bbb91cebb2a062b11a41b4c71d21e
dsurviver <dsurviver@c380b020-2878-11df-a8bb-8b6e25e4c629>
2010-06-27 17:34:34 +0000

    muita coisa nova

--- FinancialManager2/grails-app/controllers/UsuarioController.groovy
+++ FinancialManager2/grails-app/controllers/UsuarioController.groovy
@@ -1,8 +1,13 @@
 class UsuarioController {
 	
-    def login = {
-    		
-      }
-    
-    
+    def doLogin = {
+    	def usuario = Usuario.findWhere(userName:params["userName"],
+    	            senha:params["senha"])
+    	session.usuario = usuario
+    	if (usuario) {
+            redirect(controller:"categoria", action:"listAll")
+    	}
+    	
+    }
+
 }
