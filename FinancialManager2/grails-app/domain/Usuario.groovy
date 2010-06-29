8575e05f458237eed73014e15841ef49bbbd105a
arthurdesribeiro <arthurdesribeiro@c380b020-2878-11df-a8bb-8b6e25e4c629>
2010-06-29 23:04:42 +0000

    Único userName

--- FinancialManager2/grails-app/domain/Usuario.groovy
+++ FinancialManager2/grails-app/domain/Usuario.groovy
@@ -1,6 +1,11 @@
 
 class Usuario {
-	String nome
-	String userName
-	String senha
+    String nome
+    String userName
+    String senha
+    static constraints = {
+        userName(length:5..15, unique:true)
+        senha(length:4..20)
+    }
+
 }
