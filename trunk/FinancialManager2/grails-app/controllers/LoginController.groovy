248b01e055fe22ef2e228feec6b965814189f4bf
arthurdesribeiro <arthurdesribeiro@c380b020-2878-11df-a8bb-8b6e25e4c629>
2010-06-30 06:58:15 +0000

    Parte de login pronta, com logout funcionando e mensagens de warning para usuario ou senha inválidos...

--- trunk/FinancialManager2/grails-app/controllers/LoginController.groovy
+++ trunk/FinancialManager2/grails-app/controllers/LoginController.groovy
@@ -1,16 +1,38 @@
 class LoginController {
 
+    def index = {redirect(action:doLogin)}
+
     def doLogin = {
-        if (session.usuario) {
-            redirect(controller:"categoria", action:"listAll")
-        } else {
-            def usuario = Usuario.findWhere(userName:params["userName"],
-                senha:params["senha"])
-            session.usuario = usuario
-            if (usuario) {
+        if (request.method == "GET") {
+            if(session.usuario) {
                 redirect(controller:"categoria", action:"listAll")
+            } else {
+                session.usuario = null
+                def usuario = new Usuario()
+            }
+        } else {
+            if (session.usuario) {
+                redirect(controller:"categoria", action:"listAll")
+            } else {
+                def usuario = Usuario.findWhere(userName:params["userName"],
+                    senha:params["senha"])
+
+                session.usuario = usuario
+                if (usuario) {
+                    redirect(controller:"categoria", action:"listAll")
+                } else {
+                    flash['message'] = 'Usuário ou senha inválidos!'
+                }
             }
         }
     }
+
+        def logout = {
+            if (session.usuario) {
+                session.usuario = null
+                flash['message'] = 'Saiu da conta com sucesso!'
+            }
+            redirect(controller:'login', action:'doLogin')
+        }
 
 }
