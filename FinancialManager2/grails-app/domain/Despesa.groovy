6612a2fc0fec9188f672daf7b0c9ec43fcc8ab32
arthurdesribeiro <arthurdesribeiro@c380b020-2878-11df-a8bb-8b6e25e4c629>
2010-06-30 04:24:20 +0000

    Despesa não existe sem categoria

--- FinancialManager2/grails-app/domain/Despesa.groovy
+++ FinancialManager2/grails-app/domain/Despesa.groovy
@@ -1,17 +1,18 @@
 
 class Despesa {
 
+
 	String nome
 	Float valor
 	String descricao
 	Date data
 	Boolean aberta
-	
+        Categoria categoria
+	static belongsTo = Categoria
+        
 	String toString() {
 		"$data : $nome - $valor"
 	}
-
-        static belongsTo = Categoria
 //	
 //	void fecharDespesa() {
 //		
