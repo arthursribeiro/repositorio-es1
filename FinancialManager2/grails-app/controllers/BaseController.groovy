6456019244632b8fd347f574dc5e9578e589e64d
arthurdesribeiro <arthurdesribeiro@c380b020-2878-11df-a8bb-8b6e25e4c629>
2010-07-01 06:06:37 +0000

    lembro mais não

--- FinancialManager2/grails-app/controllers/BaseController.groovy
+++ FinancialManager2/grails-app/controllers/BaseController.groovy
@@ -1,8 +1,28 @@
 abstract class BaseController {
     def auth() {
+        def originalRequestParams =
+                [controller:controllerName,
+                action:actionName]
+        originalRequestParams.putAll(params)
+        session.originalRequestParams =
+                originalRequestParams
         if(!session.usuario) {
             redirect(controller:'login',action:'doLogin')
             return false
         }
+        return true
+    }
+
+    def authMaster() {
+        
+        def redirectParams = session.originalRequestParams
+        if(this.auth()) {
+            if(session.usuario.class == UsuarioMaster) {
+                return true
+            }
+            flash['message'] = 'VocÊ não possui acesso ao recurso solicitado.'
+            redirect(redirectParams)
+        }
+        return false
     }
 }
