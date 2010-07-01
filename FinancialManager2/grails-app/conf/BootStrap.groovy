41f32ed8b3781412ab00b5a581315cb14b743811
arthurdesribeiro <arthurdesribeiro@c380b020-2878-11df-a8bb-8b6e25e4c629>
2010-07-01 05:23:44 +0000

    Separando usuario comum de usuario master

--- FinancialManager2/grails-app/conf/BootStrap.groovy
+++ FinancialManager2/grails-app/conf/BootStrap.groovy
@@ -1,8 +1,8 @@
 class BootStrap {
 
      def init = { servletContext -> 
-     new Usuario(nome:"Felipe",userName:"felipevieira",senha:"ronaldinha").save()
-     new Usuario(nome:"Amaury", userName: "amaury", senha:"reby").save()
+     new UsuarioMaster(nome:"Felipe",userName:"felipevieira",senha:"ronaldinha").save()
+     new UsuarioComum(nome:"Amaury", userName: "amaury", senha:"reby").save()
      }
      def destroy = {
      }
